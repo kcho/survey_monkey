7ea77f4a6b8451d29017ec346f218fa430d7216f
kangik <kangik@neuroimage.snu.ac.kr>
2016-05-04 19:37:21 +0900

    question name ELSQ changed

--- SM_to_CCNC.py
+++ SM_to_CCNC.py
@@ -5,85 +5,83 @@ import numpy as np
 import openpyxl
 import argparse
 import textwrap
-
-
 # Set pandas options
 pd.set_option('max_rows',5000)
 pd.set_option('max_columns',5000)
 
 def main(args):
-    # import merged template
-    group_template_df = pd.read_excel(args.groupTemplate)
-    template_df = group_template_df.T.reset_index()
+
+    # Load survey monkey template with question names
+    template_df = pd.read_excel(args.groupTemplate).T.reset_index()
     template_df.columns = ['title','subtitle','question name','answers']
     template_df.ix[:8,'question name'] = 'personal_info'
 
-
     # Survey monkey data
-    all_sheets = pd.concat([pd.read_excel(x) for x in args.inputExcel],axis=1)
-    df = all_sheets.T.reset_index()
-    df.columns = ['title','subtitle','answers']
-    df.title = df.title.str.replace('<[^>]+>','')
-    df.title = df.title.map(unicode)
+    '''Add a subject selection part later'''
+    all_sheets = pd.concat([pd.read_excel(x, index_col=range(9)) for x in args.inputExcel],
+                           axis=1).reset_index()
+    real_df = all_sheets.T.reset_index()
+    real_df.columns = ['title','subtitle','answers']
+
+    real_df.title = real_df.title.str.replace('<[^>]+>','')
+    real_df.title = real_df.title.map(unicode)
 
 
     # template df 에서 question name 가지고오기
-    df['question name'] = template_df['question name']
+    real_df['question name'] = template_df['question name']
 
 
     # Formula template
     template_openpyxl = openpyxl.load_workbook(args.template, data_only=False)
-    #out_excel = openpyxl.load_workbook('/Users/kangik/Desktop/xlrd_prac.xls', data_only=False)
 
-    #print df.head(10)
-    for num, sheet_name in enumerate(template_openpyxl.sheetnames[:-5]): # 뒤에서 5개를 제외한 템플레이트의 모든 sheet를 looping
+    for num, question_name in enumerate(template_openpyxl.sheetnames[:-5]): # 뒤에서 5개를 제외한 템플레이트의 모든 sheet를 looping
         # 엑셀파일에 데이터가 바로 들어가는 것이 아니고,
         # 행 열, 4칸 3칸씩 띄우고 데이터가 들어감
         startRow = 5
         startCol = 4
 
-        sheet = template_openpyxl.get_sheet_by_name(sheet_name) # sheet를 불러옴
-        data = df.ix[df['question name'].str.strip()==sheet_name] # 서베이멍키에서 불러온 자료를 불러옴
+        sheet = template_openpyxl.get_sheet_by_name(question_name) # sheet를 불러옴
+        data = real_df.ix[real_df['question name'].str.strip()==question_name] # 서베이멍키에서 불러온 자료를 불러옴
         
-        if sheet_name == u'기본정보':
+        if question_name == u'기본정보':
             startRow -= 1
             startCol -= 1
-            for answer in [df.ix[df.title == u'응답자 ID', 'answers'].get_values()[0],
-                            str(df.ix[df.title == u'성', 'answers'].get_values()[0])+ str(df.ix[df.title == u'이름', 'answers'].get_values()[0]),
-                            df.ix[df.title == u'사용자 정의 데이터', 'answers'].get_values()[0],
-                            str(df.ix[df.title == u'시작 날짜', 'answers'].get_values()[0])[:10],
-                            df.ix[df.title == u'IP 주소', 'answers'].get_values()[0],
-                            df.ix[df.title == u'컬렉터 ID', 'answers'].get_values()[0],
-                            str(df.ix[df.title == u'종료 날짜', 'answers'].get_values()[0])[:10]]:
-                print answer
-                sheet.cell(row = startRow, column = startCol).value = str(answer)
+
+            info_list = [u'응답자 ID', u'이름', 
+                         u'사용자 정의 데이터', u'시작 날짜', 
+                         u'IP 주소', u'컬렉터 ID', u'종료 날짜']
+
+            for info, answer in [(x,str(real_df.ix[real_df.title==x,'answers'].get_values()[0])) for x in info_list]:
+                if u'날짜' in info:
+                    answer = answer[:10]
+                try:
+                    sheet.cell(row = startRow, column = startCol).value = answer
+                except:
+                    sheet.cell(row = startRow, column = startCol).value = ''
                 startRow+=1
 
 
-        elif sheet_name == '(H, I) SCL, EF': #이런 이름을 가진 sheet는
+        elif question_name == '(H, I) SCL, EF': #이런 이름을 가진 sheet는
             for num,answer in enumerate(data.answers[:29]): #처음 29줄을 프린트하고
                 sheet.cell(row = startRow, column=startCol).value =  answer
                 sheet.cell(row=startRow, column=startCol+1).value = answer
                 startRow+=1
+
             startRow+=4 #세칸 띄우고
+
             for num,answer in enumerate(data.answers[29:]): #나머지를 프린트
                 sheet.cell(row=startRow, column=startCol).value =  answer
                 sheet.cell(row=startRow, column=startCol+1).value = answer
                 startRow+=1
 
-        elif sheet_name == '(8) ELSQ': #이런 이름을 가진 sheet는
-            columns = [startCol, startCol+1]*(len(data.answers)/2) #3, 4, 3, 4가 반복되는 리스트를 만듬 (column이 3,4 순으로 들어감)
-            initialC = 3
-            for num, column in enumerate(columns):
-                if column == 3 and data.answers.tolist()[num]==0: #column 3에 들어갈 자료가 0이면
-                    sheet.cell(row=startRow, column=column).value = data.answers.tolist()[num] # 0을 쓰고
-                    sheet.cell(row=startRow, column=column+1).value = '' # 그 옆에는 '' 빈칸을 넣는다
-                elif column == 3 and answer!=0: #column 3에 들어갈 자료가 0이 아니면
-                    sheet.cell(row=startRow, column=column).value = data.answers.tolist()[num] 
-                    sheet.cell(row=startRow, column=column+1).value = data.answers.tolist()[num+1] # 그 옆에 자료를 쓴다
+        elif question_name == '(8) ELSQ': #이런 이름을 가진 sheet는
+            # 3, 4, 3, 4가 반복되는 리스트를 만듬 (column이 3,4 순으로 들어감)
+            columns = [startCol, startCol+1] * (len(data.answers)/2) 
+            # make double numbered row list
+            rows = sorted(range(startRow, len(data.answers)) * 2)
 
-                if column > initialC:
-                    startRow+=1 # 2 data point가 지날 때마다 한줄씩 증가됨
+            for num, (row, column, answer) in enumerate(zip(rows, columns, data.answers)):
+                sheet.cell(row=row, column=column).value = answer#데이터 입력
 
         else:
             for answer in data.answers:# 서베이멍키에서 불러온 각 문제의 자료들을 looping
@@ -92,7 +90,10 @@ def main(args):
                 startRow +=1
 
     template_openpyxl.save(args.output)
+
+    print '=========='
     print 'Completed'
+    print '=========='
 
 
 
